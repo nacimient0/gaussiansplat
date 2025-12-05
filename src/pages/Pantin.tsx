fb02f8a678d9e57bc3ef70678c4fe0749e3b8206
Sereir <nacim.sereir@ynov.com>
2025-12-05 15:27:49 +0100

    changement bg

--- src/pages/Pantin.tsx
+++ src/pages/Pantin.tsx
@@ -3,9 +3,9 @@
 
 import * as pc from "playcanvas";
 import { Application, Entity } from "@playcanvas/react";
-import { Camera, GSplat, Script } from "@playcanvas/react/components";
+import { Camera, GSplat, Script, Environment } from "@playcanvas/react/components";
 import { OrbitControls } from "@playcanvas/react/scripts";
-import { useSplat } from "@playcanvas/react/hooks";
+import { useSplat, useAsset } from "@playcanvas/react/hooks";
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import Credits from "../components/Credits";
 import FPSCounterScript from "../scripts/FPSCounterScript";
@@ -66,6 +66,7 @@ function Loader({ progress }) {
 /* ------------------------ Splat Scene ------------------------ */
 const SplatScene = React.memo(() => {
     const { asset, loading } = useSplat("/pantin/pantin.sog");
+    const sky = useAsset("/pantin/bg-pantin.webp", "texture");
     const progress = loading ? 0 : 1;
 
     if (!asset) return <Loader progress={progress} />;
@@ -74,8 +75,12 @@ const SplatScene = React.memo(() => {
         <>
 
             <Loader progress={progress} />
-            <Entity name="splat" position={[0, 0, 0]} rotation={[180, -3, 0]}>
+            <Entity name="splat" position={[0, 0, 0]} rotation={[180, 0, 0]}>
                 <GSplat asset={asset} />
+            </Entity>
+
+            <Entity name="skybox">
+                <Environment skybox={sky.asset} skyboxIntensity={1.25} exposure={1} />
             </Entity>
 
             {/* 🎯 FPS Counter */}
@@ -152,7 +157,7 @@ export default function Pantin() {
                     }}
                 >
                     {/* 📷 Camera avec OrbitControls et AutoRotator */}
-                    <Entity name="camera" position={[0.75, 0.35, -1.75]}>
+                    <Entity name="camera" position={[-2, 1.75, -1.75]}>
                         <Camera fov={50} clearColor="black" />
 
                         <OrbitControls
